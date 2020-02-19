657fecf6c72e52fc8977646f7cd43858b410d950
Lucy Silver <lucy.silver2@gmail.com>
2020-02-19 15:20:12 +0000

    exercise two

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+category = ["chinese", "italian", "japanese", "french", "belgian"]
+
+5.times { Restaurant.create(name: Faker::Name.name, address: Faker::Address.street_address, category: category.sample, phone_number: Faker::PhoneNumber) }
